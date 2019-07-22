82c726693c3bcddc8472070d81c5d209a50783c7
faithngotho <faithwangari248@gmail.com>
2019-07-22 12:33:42 +0300

    add added statement to the settings.py

--- gallery/views.py
+++ gallery/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
 from django.http  import HttpResponse
+import datetime as dt
+from django.http  import HttpResponse,Http404
 
 # Create your views here.
 def welcome(request):
     return HttpResponse('Welcome to My Own Gallery')
+
+def search_results(request):
+
+    if 'article' in request.GET and request.GET["article"]:
+        search_term = request.GET.get("article")
+        searched_articles = Article.search_by_title(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'all-gallery/search.html',{"message":message,"articles": searched_articles})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'all-gallery/search.html',{"message":message})
